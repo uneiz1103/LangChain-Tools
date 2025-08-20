c553c60714302be1b1dab632a6e11459652c2060
uneiz1103 <uneizshaikh1103@gmail.com>
2025-08-20 15:39:23 +0530

    feat: add DuckDuckGo search tool example for daily news

--- langchain_tools_demo.py
+++ langchain_tools_demo.py
@@ -1,6 +1,6 @@
 from langchain_community.tools import DuckDuckGoSearchResults
 from langchain_community.utilities.duckduckgo_search import DuckDuckGoSearchAPIWrapper
 
-api = DuckDuckGoSearchAPIWrapper(source="news", time="d", max_results=5)  # d = day
-news_tool = DuckDuckGoSearchResults(api_wrapper=api, output_format="list")  # or "json"
+api = DuckDuckGoSearchAPIWrapper(source="news", time="d", max_results=5)
+news_tool = DuckDuckGoSearchResults(api_wrapper=api, output_format="list")  
 print(news_tool.invoke("rainfall India today"))
