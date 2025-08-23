1201056c4bac98ab5633a721d39380f15222955e
uneiz1103 <uneizshaikh1103@gmail.com>
2025-08-23 23:33:31 +0530

    Add ReAct agent with DuckDuckGo search integration

--- agents_in_langchain.py
+++ agents_in_langchain.py
@@ -1,10 +1,48 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.tools import tool
 from langchain_community.tools import DuckDuckGoSearchRun
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain import hub
 import requests
 
 search_tool = DuckDuckGoSearchRun()
 
-results = search_tool.invoke('top news in india today')
+results = search_tool.invoke('who has win ipl this year in india')
+# print(results)
 
-print(results)
+llm = ChatOpenAI()
+
+# step 2: pull the ReAct prompt from LangChain Hub
+prompt = hub.pull("hwchase17/react") #pulls the standard ReAct agent prompt
+
+
+# step 3: Create the ReAct agent manually with the pulled prompt
+agent = create_react_agent(
+    llm=llm,
+    tools=[search_tool],
+    prompt=prompt
+)
+# step 4: Wrap it with AgentExecutor
+agent_executor = AgentExecutor(
+    agent=agent,
+    tools=[search_tool],
+    verbose=True
+)
+
+response = agent_executor.invoke({"input": "3 ways to reach goa from delhi"})
+# print(response)
+# print(response['output'])
+# Thought , action, observation
+
+# Entering new AgentExecutor chain...
+# Thought: The user is asking two related questions.
+# Action: Use knowledge to find the capital of France.
+# Observation: The capital of France is Paris.
+# Thought: Now I need the population of Paris.
+# Action: Search API[ "population of Paris 2025" ]
+# Observation: Around 2.1 million people (city proper).
+# Thought: I now have both answers.
+
+
+
+
